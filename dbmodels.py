4e7ee47c49b591e1bd4bff522497225597931b73
Shibiliya Ismail <shibiliya.ismail@thiswillfloat.org>
2021-06-18 12:43:06 +0000

    Database Model List

---
+++ dbmodels.py
@@ -0,0 +1,13 @@
+"""
+    User Table : User ID, Username, First Name, Last Name, Email, Password(Hashed), is_active, Contact, Roles etc. 
+
+    Session Table : Session ID, User ID, Token ID etc
+
+    Logging : User ID, App ID, Entry time, Exit Time
+
+    App Table : App ID, App Secret(Hashed), App Name, Email, User ID (many to many)
+
+    Token Table : User ID, App ID, Token ID
+    
+     
+"""
