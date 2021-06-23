880c494eafdd3191143449a3054336b74254351c
Royal <123royalbabu@gmail.com>
2021-06-23 20:09:44 +0530

    Done simple Fastapi end-point at root closes #3

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # endpoints are updated in the readme
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get('/')
+def root():
+    detail = {
+        "name": "Symetry API",
+        "description" : "Symertry SSO general API",
+        "version": "0.0.1",
+        "origin" : "Float Business Accelerator",
+        "team" : "Monsoon '21 Batch"
+        }
+
+    return detail
