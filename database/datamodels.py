1ad08033ad1bb4907dd1e61e38ba1c5a277092ec
aksasthomas <32041172+aksathomas@users.noreply.github.com>
2021-06-24 15:28:44 +0530

    added app schemas

--- database/datamodels.py
+++ database/datamodels.py
@@ -2,3 +2,23 @@
 from typing import List, Optional
 
 from pydantic import BaseModel
+
+class CreateApp(BaseModel):
+    """Create App Schema"""
+    user_id: str
+    name: str
+    email: str
+    contact: str
+    app_name:str
+
+class App(CreateApp):
+    """Read App Schema"""
+    app_id: str
+    app_secret: str
+    
+class Config:
+        """Enable ORM mode"""
+        orm_mode = True
+
+
+
