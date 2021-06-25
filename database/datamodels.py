29a57bc5641364f76a1c8ffa8c41e2121134fc10
Shubhangi  Pillai <shubhangi.pillai@thiswillfloat.org>
2021-06-25 18:34:29 +0000

    Update datamodels.py

--- database/datamodels.py
+++ database/datamodels.py
@@ -1,4 +1,12 @@
-#schemas
 from typing import List, Optional
 
 from pydantic import BaseModel
+class TokenSchema(BaseModel):
+    #  session schema
+    app_id: int
+    user_id: int
+    token_id: str
+
+    class Config:
+        # Enable ORM mode
+        orm_mode = True
