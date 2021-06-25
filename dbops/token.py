46d138900da0731567b5b2e5f7186b09031e0d6a
Shubhangi  Pillai <shubhangi.pillai@thiswillfloat.org>
2021-06-25 13:04:59 +0000

    Replace token.py

--- dbops/token.py
+++ dbops/token.py
@@ -1,46 +1,35 @@
-
-from sqlalchemy.orm import Token
-from database import models, datamodels
-
-
-def get_Token( db: Session, id: int):
-     return db.query(models.Token).filter(models.Token.id == id).first()
-
-def get_user_id(db: Session, skip: int = 0, limit: int = None, identify_by=dict , sort_by="id", sort_order="asc"):
-    if limit is None:
-        return db.query(models.Token).filter_by(**identify_by).offset(skip).order_by("%s %s" % (sort_by, sort_order)).all()
-
-    return db.query(models.Token).filter_by(**identify_by).offset(skip).limit(limit).order_by("%s %s" % (sort_by, sort_order)).all()
-
-def update_Token(ddb: Session, user_id,user_update_data:datamodels.UserUpdate ):
-    query=db.query(models.Tokenr).filter_by(user_id= user_id).first()
-    for var, value in vars(user_update_data).items():
-        if value:
-            setattr(query, var, value)
-    db.add(query)
-    db.commit()
-    db.refresh(query)
-
-    return query
-
-    
-
-    def create_Token(db: Session, Token: datamodels.CreateToken):
-   
-    db_Token= models.Token(**models.Token.dict())
-    db.add(db_Token)
-    db.commit()
-    db.refresh(db_Token)
-    
-    return db_Token
-
-def delete_Token(user_id , db: Session):
-    query=db.query(models.Token).filter_by(user_id= user_id).first()
-    query.is_active=False
-    db.add(query)
-    db.commit()
-    db.refresh(query)
+from sqlalchemy.orm import Session
+from dbops.common import commit_changes_to_object
+from database.models import Token
+from database.datamodels import TokenSchema
 
 
+def get_token_id(
+        db: Session,
+        session: TokenSchema):        # get token id 
+
+    return db.query(Token).filter(Token.user_id == session.user_id, Token.app_id == session.app_id).first()
 
 
+def get_token_id_by_app_id(
+        db: Session,
+        app_id: int):                   # get token id with app id
+
+    return db.query(Token).filter_by(app_id == app_id).all()
+
+
+def get_token_id_by_user_id(
+        db: Session,
+        user_id: int):                  # get token id with user id
+
+    return db.query(Token).filter_by(user_id == user_id).all()
+
+
+def create_token_id(
+        database: Session,
+        session: TokenSchema):        # create session with user id
+
+    user_session = Token(**session.dict())
+    commit_changes_to_object(database, user_session)
+
+    return user_session
