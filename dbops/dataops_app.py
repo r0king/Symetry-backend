0ebb578a702c88d08c5f92468aad196a916a1ce6
aksasthomas <32041172+aksathomas@users.noreply.github.com>
2021-06-25 13:53:28 +0530

    finished update

--- dbops/dataops_app.py
+++ dbops/dataops_app.py
@@ -4,7 +4,7 @@ from dbops.common import commit_changes_to_object
 from database.models import App
 from database.datamodels import CreateApp
 
-def create_app(db:Session, app:CreateApp, ):
+def create_app(db:Session, app:CreateApp):
     """To create a new entry"""
     db_app=App(**app.dict())
     if db_app:
@@ -15,7 +15,7 @@ def create_app(db:Session, app:CreateApp, ):
 
 def delete_app(db:Session, app:App, user_id:int):
     """To delete an app entry"""
-    query=db.query(app).filter_by(app.user_id==user_id)
+    query=db.query(app).filter(app.user_id==user_id)
     if query:
    	    raise HTTPException(status_code=409, detail="App not found.")
     db.delete(query)
@@ -23,9 +23,17 @@ def delete_app(db:Session, app:App, user_id:int):
 
 def update_app(db:Session, app:App, user_id:int):
     """To update the app profile"""
-    query=db.query(app).filter_by(app.user_id== user_id)
+    query=db.query(app).filter(app.user_id== user_id)
     if query:
    	    raise HTTPException(status_code=409, detail="App not found.")
+    for i,j in vars(app.dict()):
+    db.execute(update(app).values(j="")
+    db.add(query)
+    db.commit()
+    return query
+
+
+    
     
 def get_all_apps(db: Session, limit = None, identify_by: dict = dict,offset: int = 0, sort_by: str = "user_id", order: str = "asc"):
     """Displays all the app profiles"""
