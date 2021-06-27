734392e7194a2bcc9e700fc7fcaedc2d1c309820
Shubhangi  Pillai <shubhangi.pillai@thiswillfloat.org>
2021-06-27 12:24:47 +0000

    Update token.py

--- dbops/token.py
+++ dbops/token.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy.orm import Session
 from dbops.common import commit_changes_to_object
 from database.models import Token
@@ -15,14 +16,14 @@ def get_token_id_by_app_id(
         db: Session,
         app_id: int):                   # get token id with app id
 
-    return db.query(Token).filter_by(app_id == app_id).all()
+    return db.query(Token).filter_by(app_id == app_id)
 
 
 def get_token_id_by_user_id(
         db: Session,
         user_id: int):                  # get token id with user id
 
-    return db.query(Token).filter_by(user_id == user_id).all()
+    return db.query(Token).filter_by(user_id == user_id)
 
 
 def create_token_id(
@@ -33,3 +34,12 @@ def create_token_id(
     commit_changes_to_object(database, user_session)
 
     return user_session
+def delete_token(
+        db: Session,
+        session: TokenSchema):        # delete token id 
+
+    query = db.query(Token).filter(Token.user_id == session.user_id, Token.app_id == session.app_id).first()
+    db.delete(query)
+    db.commit()
+    db.refresh(query)
+    return query
