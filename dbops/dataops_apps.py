c9818290852cc1b4b98312f5197d6115c86f7139
aksasthomas <32041172+aksathomas@users.noreply.github.com>
2021-06-28 03:23:15 +0530

    resolved merge conflicts

--- dbops/dataops_apps.py
+++ dbops/dataops_apps.py
@@ -13,20 +13,40 @@ def create_app(db:Session, app:CreateApp):
     commit_changes_to_object(db, db_app)
     return db_app
 
-def delete_app(db:Session, app:App, user_id:int):
+def delete_app(db:Session, app:App, app_id:str):
     """To delete an app entry"""
-    query=db.query(app).filter(app.user_id==user_id)
-    if query:
+    query=db.query(App).filter(app.app_id==app_id)
+    if query is None:
    	    raise HTTPException(status_code=409, detail="App not found.")
     db.delete(query)
     commit_changes_to_object(db, query)
 
-def update_app(db:Session, app:App, update_app:updateApp, user_id:int):
+def update_app(db:Session, app:App, update_app:updateApp, user_id:str):
     """To update the app profile"""
-    query=db.query(app).filter(app.user_id== user_id)
+    query=db.query(App).filter(app.user_id== user_id)
     if query is None:
-        return None
-    for name,entry in vars(app.dict()):
+         raise HTTPException(status_code=409, detail="App not found.")
+    for name,entry in vars(app.dict(exclude_none=True)):
         db.execute(update(query.entry).values(updateApp.entry))
     commit_changes_to_object(db, query)
     return query
+
+def get_apps(db: Session, app_id: str, user_id: str, name: str, app_name:str, limit = None, identify_by: dict = dict, offset: int = 0, sort_by: str = "user_id", order: str = "asc"):
+    """Displays all the app profiles"""
+    query = db.query(App).filter_by(**identify_by).offset(offset).limit(limit).\
+        order_by("%s %s" % (sort_by, order))
+    
+    if user_id:
+        return db.query(App).filter(App.user_id == user_id).all()
+    
+    if name:
+        return db.query(App).filter(App.name == name).all()
+    
+    if app_id:
+        return db.query(App).filter(App.app_id == app_id).all()
+    
+    if app_name:
+        return db.query(App).filter(App.app_name == app_name).first()
+    return query.all()
+
+
