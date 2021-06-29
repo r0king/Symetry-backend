222a5500f1a2663aa28bec989d374cc5328bdecf
Royal <123royalbabu@gmail.com>
2021-06-29 12:26:38 +0000

    Changed api endpoint listing pos

--- app/main.py
+++ app/main.py
@@ -2,23 +2,10 @@
 from fastapi import FastAPI
 
 
-# GET        /                      Get root information about the APIreturn [details ]
-# GET        /auth/user/            Get Current User Inforeturn [user ]
-# POST       /auth/user/            Create a new User (Registration)return [user ]
-# PATCH      /auth/user/{user_id}/  Update Existing User Inforeturn [user  ]
-# DELETE     /auth/user/{user_id}/  Soft Delete User by IDreturn [user  ]
-# POST       /auth/login/           Return tokenID by submitting credentialsreturn ["token_id": STRING  ]
-# POST       /auth/validate/        Creates a session by submitting tokenIDreturn [{"token": STRING, "type": STRING}  ]
-# POST       /auth/check/           Checks if a token is validreturn ["status": BOOLEAN  ]
-# POST       /auth/logout/          Terminates the sessionreturn [loged out sussesfully ]
-# POST       /auth/app              Create a new App (Registration)return [app  ]
-# POST       /auth/app/login/       Creates a session by submitting credentialsreturn ["token": STRING  ]
-# PATCH      /auth/app/{app_id}/    Update Existing App Inforeturn [app ]
-# DELETE     /auth/app/{app_id}/    Soft Delete App by IDreturn [app ]
-# GET        /log/                  Gets the logs updated till thenreturn [log ]
 
 app = FastAPI()
 
+# GET        /                      Get root information about the APIreturn [details ]
 
 @app.get('/')
 def root():
@@ -31,3 +18,28 @@ def root():
     }
 
     return detail
+# GET        /auth/user/            Get Current User Inforeturn [user ]
+
+# POST       /auth/user/            Create a new User (Registration)return [user ]
+
+# PATCH      /auth/user/{user_id}/  Update Existing User Inforeturn [user  ]
+
+# DELETE     /auth/user/{user_id}/  Soft Delete User by IDreturn [user  ]
+
+# POST       /auth/login/           Return tokenID by submitting credentialsreturn ["token_id": STRING  ]
+
+# POST       /auth/validate/        Creates a session by submitting tokenIDreturn [{"token": STRING, "type": STRING}  ]
+
+# POST       /auth/check/           Checks if a token is validreturn ["status": BOOLEAN  ]
+
+# POST       /auth/logout/          Terminates the sessionreturn [loged out sussesfully ]
+
+# POST       /auth/app              Create a new App (Registration)return [app  ]
+
+# POST       /auth/app/login/       Creates a session by submitting credentialsreturn ["token": STRING  ]
+
+# PATCH      /auth/app/{app_id}/    Update Existing App Inforeturn [app ]
+
+# DELETE     /auth/app/{app_id}/    Soft Delete App by IDreturn [app ]
+
+# GET        /log/                  Gets the logs updated till thenreturn [log ]
